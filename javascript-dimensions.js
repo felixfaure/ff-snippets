5ab1fcabc325ec6c4f907c8cd83972f42be6f6a0
David Félix-Faure <contact@felixfaure.fr>
2016-04-27 01:29:31 +0200

    Add dimensions js

--- javascript-dimensions.js
+++ javascript-dimensions.js
@@ -24,6 +24,24 @@ var width = el.offsetWidth;
 //Hauteur/largeur interne (+ scrollbar) d'un élément IE9+ (pour cross-browser voir window.innerHeight plus haut)
 var height = el.innerHeight;
 var width = el.innerWidth;
+//Hauteur/largeur du contenu même si scroll
+var height = el.scrollHeight;
+var width = el.scrollWidth;
+//Hauteur/largeur avec marges
+function outerWidth(el) {
+  var width = el.offsetWidth;
+  var style = getComputedStyle(el);
+  width += parseInt(style.marginLeft) + parseInt(style.marginRight);
+  return width;
+}
+outerWidth(el);
+function outerHeight(el) {
+  var height = el.offsetHeight;
+  var style = getComputedStyle(el);
+  height += parseInt(style.marginTop) + parseInt(style.marginRight);
+  return height;
+}
+outerHeight(el);
 
 //Scroll vertical/horizontal d'un élément
 var scrollY = el.scrollTop;
