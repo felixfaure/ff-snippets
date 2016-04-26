8a4aefd52dfb42f181b7888ebed2db946f7dbcf0
David Félix-Faure <contact@felixfaure.fr>
2016-04-27 01:30:07 +0200

    Add a vanilla javascript scrollTo function

---
+++ javascript-scrollto.js
@@ -0,0 +1,43 @@
+//Easing functions (inspired by http://gizma.com/easing/)
+var easing = {
+  linear: function (t) { return t },
+  easeInQuad: function (t) { return t*t },
+  easeOutQuad: function (t) { return t*(2-t) },
+  easeInOutQuad: function (t) { return t<.5 ? 2*t*t : -1+(4-2*t)*t },
+  easeInCubic: function (t) { return t*t*t },
+  easeOutCubic: function (t) { return (--t)*t*t+1 },
+  easeInOutCubic: function (t) { return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1 },
+  easeInQuart: function (t) { return t*t*t*t },
+  easeOutQuart: function (t) { return 1-(--t)*t*t*t },
+  easeInOutQuart: function (t) { return t<.5 ? 8*t*t*t*t : 1-8*(--t)*t*t*t },
+  easeInQuint: function (t) { return t*t*t*t*t },
+  easeOutQuint: function (t) { return 1+(--t)*t*t*t*t },
+  easeInOutQuint: function (t) { return t<.5 ? 16*t*t*t*t*t : 1+16*(--t)*t*t*t*t }
+}
+
+//ScrollTo animate
+function scrollTo(Y, duration, easingFunction, callback) {
+  var start = Date.now(),
+      from = window.pageYOffset;
+
+  if(from === Y) {
+    if(callback) callback();
+    return;
+  }
+
+  if(!easingFunction) easingFunction = easing.linear;
+
+  function scroll(timestamp) {
+    var currentTime = Date.now(),
+        time = Math.min(1, ((currentTime - start) / duration)),
+        easedT = easingFunction(time);
+
+    yPos = (easedT * (Y - from)) + from;
+    window.scrollTo(0, yPos);
+
+    if(time < 1) requestAnimationFrame(scroll);
+    else if(callback) callback();
+  }
+
+  requestAnimationFrame(scroll);
+}
