33606cf42a89c5d1521e016c2f13eb90f434994b
David Félix-Faure <contact@felixfaure.fr>
2016-08-25 12:04:55 +0200

    JS : better history support test

--- javascript-support.js
+++ javascript-support.js
@@ -9,17 +9,19 @@ function support_css_filter() {
 //Object-fit
 supports.objectFit = ('objectFit' in html.style === false) ? false : true;
 
-
 //History
 var ua = navigator.userAgent;
 function test_history() {
-  if ((ua.indexOf('Android 2.') !== -1 || ua.indexOf('Android 4.0') !== -1) &&
+  if((ua.indexOf('Android 2.') !== -1 || ua.indexOf('Android 4.0') !== -1) &&
       ua.indexOf('Mobile Safari') !== -1 &&
       ua.indexOf('Chrome') === -1 &&
       ua.indexOf('Windows Phone') === -1) {
     return false;
   }
-  return (window.history && 'pushState' in window.history);
+  if(ua.match(/((iPod|iPhone|iPad).+\bOS\s+[1-4]|WebApps\/.+CFNetwork)/)) {
+    return false;
+  }
+  return (window.history && 'pushState' in window.history && 'replaceState' in window.history);
 }
 
 //Function to test a css property
