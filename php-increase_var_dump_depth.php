f042ef1dbed03947fb007b609ff542cb3e599c8f
David Félix-Faure <contact@felixfaure.fr>
2016-08-25 12:06:06 +0200

    PHP : increase var_dump depth

---
+++ php-increase_var_dump_depth.php
@@ -0,0 +1,10 @@
+<?php
+ini_set('xdebug.var_display_max_depth', 20);
+ini_set('xdebug.var_display_max_children', 500);
+ini_set('xdebug.var_display_max_data', 2048);
+
+//No limit (maximum nesting is 1023) :
+ini_set('xdebug.var_display_max_depth', -1);
+ini_set('xdebug.var_display_max_children', -1);
+ini_set('xdebug.var_display_max_data', -1);
+?>
